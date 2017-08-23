85749c4cac9255e9e9b194f62f57f9908d78dc13
Jochen Lutz <jlu@gmx.de>
2017-08-23 15:05:58 +0200

    removed TOTORIAL comments

--- src/main/java/org/streampipes/biggis/sources/sensebox/Main.java
+++ src/main/java/org/streampipes/biggis/sources/sensebox/Main.java
@@ -3,27 +3,12 @@ package org.streampipes.biggis.sources.sensebox;
 import org.streampipes.container.init.DeclarersSingleton;
 import org.streampipes.container.standalone.init.StandaloneModelSubmitter;
 
-/*
-    TUTORIAL:
-    This is the main class of the examples sources project.
-    Here you must register all the event producers. A producer is a collection of different streams.
-    The main method starts a web server that provides all the descriptions of the registered
-    producers and their streams.
- */
 public class Main extends StandaloneModelSubmitter {
 
     public static void main(String[] args) {
-
-        /*
-            TOTORIAL:
-            Add the newly created event producer to the declarer singleton
-         */
         DeclarersSingleton.getInstance()
                 .add(new SenseboxSource());
-        /*
-            TOTORIAL:
-            First a port for the service is set and then the server is started
-         */
+
         DeclarersSingleton.getInstance().setPort(8085);
         new Main().init();
     }
